687250145c8c15a78abfb1cdb8ba04a542168bf2
BobTheBuilder <robertksam2000@gmail.com>
2021-09-30 03:05:36 -0700

    modifed app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import './App.css';
-import {useState} from 'react'
-import { useRef } from 'react/cjs/react.development';
+
 
 
 function App() {  
-  const [text, setText] = useState("")
-  const inputValue = useRef()
-  const [disabled, setDisabled] = useState(false)
-
-  const handleChange = e =>{
-    setText(e.target.value)
-  }
-  
-  const submit = () =>{
-    console.log(inputValue.current.value)
-    setDisabled(!text)
-  }
+ 
   return (
     <div className="App">
-      <input ref={inputValue} value={text} onChange={handleChange} name="name" id="name" />
-      <button disabled={disabled} onClick={submit}>Click Me</button>
+      <h1>This is the air I breathe in the air.</h1>
     </div>
   );
 }
